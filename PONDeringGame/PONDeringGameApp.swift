8a251abfc0d8923ea98f971e114997ab9a258074
triforcepotato <prieshajhala@gmail.com>
2023-07-18 10:06:13 -0700

    Initial Commit

---
+++ PONDeringGame/PONDeringGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  PONDeringGameApp.swift
+//  PONDeringGame
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct PONDeringGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
